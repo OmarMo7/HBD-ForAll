cb819f7dc9d923bf9e59afb61755a4288e3efd5c
OmarMo7 <omar7.first@gmail.com>
2021-01-18 14:31:47 +0200

    Defining variables for dom nodes

--- js/app.js
+++ js/app.js
@@ -176,7 +176,8 @@ var light_h2_Elements = h2_Elements[0].style.color;
 var light_nav_Element = nav_Element.style.backgroundColor;
 var light_section_Elements = section_Elements[0].style.color;
 var light_p_Elements = p_Elements[0].style.color;
-
+var DarkModeLabel = document.getElementById("DarkModeLabel").style.transform;
+var switchButton = document.querySelector(".switch")
 
 function lightMode() {
 
@@ -202,16 +203,16 @@ function lightMode() {
   }
 
   nav_Element.style.backgroundColor = 'rgb(211, 211, 211)';
-  document.getElementById("DarkModeLabel").innerHTML = "Light Mode"
-  document.getElementById("DarkModeLabel").style.color = 'rgb(255, 255, 255)'
-  
+  DarkModeLabel.innerHTML = "Light Mode"
+  DarkModeLabel.style.color = 'rgb(255, 255, 255)'
+
   let j = 0;
   navbar.innerHTML = '';
   sections.forEach(section => {
     j++;
     navbar.innerHTML += `<li><a href="#${section.id = 'section' + j}" style = "color: rgb(51, 51, 51)">${section.attributes['data-nav'].nodeValue}</a></li>`
   })
-  
+
 }
 
 lightMode();
@@ -241,8 +242,8 @@ function nightMode() {
   }
 
   nav_Element.style.backgroundColor = light_nav_Element;
-  document.getElementById("DarkModeLabel").innerHTML = "Night Mode"
-  document.getElementById("DarkModeLabel").color = 'rgb(27, 26, 26)'
+  DarkModeLabel.innerHTML = "Night Mode"
+  DarkModeLabel.color = 'rgb(27, 26, 26)'
 
   let j = 0;
   navbar.innerHTML = '';
@@ -259,7 +260,7 @@ function toggle() {
     nightMode();
     toggleButton.classList.toggle('checked')
     console.log('night')
-    
+
   }
   else {
     lightMode();
@@ -267,16 +268,16 @@ function toggle() {
     console.log('light')
   }
   console.log(document.body.style.background)
-console.log(nav_Element.style.backgroundColor)
+  console.log(nav_Element.style.backgroundColor)
 }
 
 
-if (screen.availWidth >= 350 && screen.availWidth < 414){
-  for (let i = 0; i < imgs.length; i++){
+if (screen.availWidth >= 350 && screen.availWidth < 414) {
+  for (let i = 0; i < imgs.length; i++) {
     imgs[i].setAttribute('style', 'width:340px; height: 380px;')
   }
-  document.getElementById("DarkModeLabel").style.transform = 'translate(-70px,21px)'
-  document.querySelector(".switch").setAttribute('style', `position: relative; display: inline-block;left: 78%;
+  DarkModeLabel.style.transform = 'translate(-70px,21px)'
+  switchButton.setAttribute('style', `position: relative; display: inline-block;left: 78%;
   width: 60px;
   height: 34px;
   top: 19%;
@@ -284,21 +285,21 @@ if (screen.availWidth >= 350 && screen.availWidth < 414){
 }
 
 
-if (screen.availWidth >= 414 && screen.availWidth < 800 ){
+if (screen.availWidth >= 414 && screen.availWidth < 800) {
   for (let i = 0; i < imgs.length; i++) {
     imgs[i].setAttribute('style', 'width:380px; height: 400px;')
   }
 
-  document.getElementById("DarkModeLabel").style.transform = 'translate(-230%,21px)'
-  document.querySelector(".switch").setAttribute('style', `position: relative; display: inline-block;left: 58%;
+  DarkModeLabel = 'translate(-230%,21px)'
+  switchButton.setAttribute('style', `position: relative; display: inline-block;left: 58%;
   width: 60px;
   height: 34px;
   top: 19%;
   transform: translateY(25px);`)
-  
+
 }
 
-if (screen.availWidth >= 800 && screen.availWidth < 1440){
+if (screen.availWidth >= 800 && screen.availWidth < 1440) {
   for (let i = 0; i < imgs.length; i++) {
     imgs[i].setAttribute('style', 'width: 768px; height: 600px;')
   }
